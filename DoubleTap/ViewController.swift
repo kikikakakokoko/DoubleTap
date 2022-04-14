c5482aa7e78549b600bad30b7c8fc3a587d3e275
ppp <ppp>
2022-04-14 18:32:09 +0900

    double tap

--- DoubleTap/ViewController.swift
+++ DoubleTap/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         let imageView = UIImageView(frame: .zero)
         imageView.contentMode = .scaleAspectFit
         
+        imageView.clipsToBounds = true
         imageView.isUserInteractionEnabled = true
         imageView.image = UIImage(named: "Image")
         return imageView
@@ -34,9 +35,36 @@ class ViewController: UIViewController {
     }
     @objc private func didDoubleTap( _ gesture: UITapGestureRecognizer) {
         
-        print("fired")
+        guard let gestureView = gesture.view else {
+            return
+        }
         
+        let size = gestureView.frame.size.width/4
+        
+        let heart = UIImageView(image: UIImage(systemName: "heart.fill"))
+        heart.frame = CGRect(x: (gestureView.frame.size.width-size)/2, y: (gestureView.frame.size.height-size)/2, width: size, height: size)
+        heart.tintColor = .white
+        heart.alpha = 0
+        
+        gestureView.addSubview(heart)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                heart.alpha = 1
+            }, completion: { done in
+                
+                if done{
+                    UIView.animate(withDuration: 1, animations: {
+                        heart.alpha = 0
+                    },completion: { done in
+                        if done {
+                            heart.removeFromSuperview()
+                        }
+                    })
+                }
+            })
+        })
     }
-
 }
 
