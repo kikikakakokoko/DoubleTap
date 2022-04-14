b7feacbae1630adc9eeea5445312efe20dda5224
ppp <ppp>
2022-04-14 14:57:51 +0900

    image plused

--- DoubleTap/ViewController.swift
+++ DoubleTap/ViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 220, height: 220))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "Image")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("ddd")
+        view.addSubview(imageView)
+        imageView.center = view.center
     }
 
 
