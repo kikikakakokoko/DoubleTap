d95fc2d1cf848e20c0fe962073796af856e648b0
ppp <ppp>
2022-04-14 15:14:35 +0900

    tap gesture added

--- DoubleTap/ViewController.swift
+++ DoubleTap/ViewController.swift
@@ -11,8 +11,10 @@ class ViewController: UIViewController {
 
     private let imageView: UIImageView = {
         
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 220, height: 220))
+        let imageView = UIImageView(frame: .zero)
         imageView.contentMode = .scaleAspectFit
+        
+        imageView.isUserInteractionEnabled = true
         imageView.image = UIImage(named: "Image")
         return imageView
     }()
@@ -20,9 +22,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        
         imageView.center = view.center
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didDoubleTap(_:)))
+        
+        tapGesture.numberOfTapsRequired = 2
+        imageView.addGestureRecognizer(tapGesture)
+        
     }
-
+    @objc private func didDoubleTap( _ gesture: UITapGestureRecognizer) {
+        
+        print("fired")
+        
+    }
 
 }
 
